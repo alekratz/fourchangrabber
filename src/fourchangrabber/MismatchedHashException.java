a984e22bb7390d076b8508e6394aa71114f4c342
Alek Ratzloff <alekratz@gmail.com>
2014-09-21 12:42:14 -0400

    Add serial UUID version thing to make Eclipse not give me warnings

--- src/fourchangrabber/MismatchedHashException.java
+++ src/fourchangrabber/MismatchedHashException.java
@@ -1,6 +1,8 @@
 package fourchangrabber;
 
 public class MismatchedHashException extends Exception {
+	private static final long serialVersionUID = 4494385183731761143L;
+
 	public MismatchedHashException(String got, String expected) {
 		super("File received with hash " + got + " but expected " + expected);
 	}
